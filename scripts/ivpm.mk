e854ae50e48e3d2eae41a0033c3d762000a27fd7
Matthew Ballance <matt.ballance@gmail.com>
2018-07-06 09:14:56 -0700

    update clean target

--- scripts/ivpm.mk
+++ scripts/ivpm.mk
@@ -33,9 +33,16 @@ RULES := 1
 
 ifeq (true,$(PHASE2))
 build : $(BUILD_DEPS)
+
+clean :
+	$(Q)rm -rf $(ROOT_DIR)/build $(ROOT_DIR)/lib
+
 else
 build : $($(PROJECT)_deps)
 	$(Q)$(MAKE) -f $(SCRIPTS_DIR)/ivpm.mk PHASE2=true VERBOSE=$(VERBOSE) build
+
+clean : $($(PROJECT)_clean_deps)
+	$(Q)$(MAKE) -f $(SCRIPTS_DIR)/ivpm.mk PHASE2=true VERBOSE=$(VERBOSE) clean
 endif
 
 # Change Makefile targets
